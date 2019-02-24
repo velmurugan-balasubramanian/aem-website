ffec057fb783ff30a3d75fd7fcc21105056babd1
Velmurugan Balasubramanian <velmurugan1124@gmail.com>
2019-02-24 23:40:30 +0530

    Mongo

---
+++ core/src/main/java/com/aem/website/core/servlets/MongoDataWriteServlet.java
@@ -0,0 +1,39 @@
+package com.aem.website.core.servlets;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.servlets.HttpConstants;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Component;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+/**
+ * Servlet that writes some sample content into the response. It is mounted for
+ * all resources of a specific Sling resource type. The
+ * {@link SlingSafeMethodsServlet} shall be used for HTTP methods that are
+ * idempotent. For write operations use the {@link SlingAllMethodsServlet}.
+ */
+@Component(service=Servlet.class,
+        property={
+                Constants.SERVICE_DESCRIPTION + "=Simple Demo Servlet",
+                "sling.servlet.paths="+ "/bin/saveFormData",
+                "sling.servlet.methods=" + HttpConstants.METHOD_GET
+
+        })
+public class MongoDataWriteServlet extends SlingSafeMethodsServlet {
+
+    private static final long serialVersionUid = 1L;
+
+    @Override
+    protected void doGet(final SlingHttpServletRequest req,
+                         final SlingHttpServletResponse resp) throws ServletException, IOException {
+        final Resource resource = req.getResource();
+        resp.setContentType("text/plain");
+        resp.getWriter().write("asdfghjk");
+    }
+}
